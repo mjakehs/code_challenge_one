10929ab84b2eeb6f91808509747654e950e6b1be
Jake Marzitelli <mjakehs@gmail.com>
2018-09-04 13:25:47 -0500

    added swap button functionality

--- client.js
+++ client.js
@@ -5,7 +5,8 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('jq');
     $('#buttonHere').append('<button id="generatorButton">Generate</button>');
-    $('#buttonHere').on('click', 'button', makeDiv);
+    $(document).on('click', '#generatorButton', makeDiv);
+    $(document).on('click', '.swapButton', bgToggle);
 }
 let timesButtonPressed = 0;
 function makeDiv() {
@@ -14,10 +15,14 @@ function makeDiv() {
     timesButtonPressed++
     //append a div with a p tag in it 
     $('#moreDivsHere').append(`
-        <div>
+        <div class="generatedDiv">
             <p>` + timesButtonPressed + `</p>
             <button class="swapButton">Swap</button>
             <button class="deleteButton">Delete</button>
         </div>
 `);
+}
+
+function bgToggle(){
+    $(this).parent().toggleClass('bgYellow');
 }
