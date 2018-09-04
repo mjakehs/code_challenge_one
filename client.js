c6046095b0676fadc5f6817c2bd926c04b9c7ef1
Jake Marzitelli <mjakehs@gmail.com>
2018-09-04 13:14:25 -0500

    added generate button and working click handler

--- client.js
+++ client.js
@@ -4,4 +4,10 @@ $( document ).ready( readyNow );
 
 function readyNow(){
     console.log('jq');
+    $('#buttonHere').append('<button id="generatorButton">Generate</button>');
+    $('#buttonHere').on('click', 'button', makeDiv);
+}
+
+function makeDiv(){
+    console.log('in makeDiv');
 }
